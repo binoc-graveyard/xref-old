ce578b6d1185f64cdad34ec8439b18a2c04b9060
Gavin Sharp <gavin@mozilla.com>
2008-06-10 15:09:41 -0400

    Bug 438371: add hg.m.o/mobile-browser, r=timeless

---
+++ tests/syntax-highlighting/continuation.h
@@ -0,0 +1,11 @@
+int conti1;
+/* line continuation tests */
+int conti2;
+
+#define CONTI3 3
+#define CONTI4(CONTI5) 6
+#define CONTI7 "x" \
+"y" \
+"z"
+
+int conti8;
---
+++ tests/syntax-highlighting/mcetool.c
@@ -0,0 +1,53 @@
+/** function parsing
+ */
+void test1()
+{
+  if (unrecognized_func_call(name) == 0))) {
+goto EXIT;
+  }
+}
+
+void recognized_function(){}
+
+void test2()
+{
+  if ((func_call() ) ) {
+goto EXIT;
+  }
+}
+
+void recognized_function2(){}
+
+void test3()
+{
+  if ((func_call() ) ) {
+  }
+}
+
+void recognized_function3(){}
+
+void broken_funct1()
+{
+  if ((func_call())) {
+  }
+}
+
+void recognized_function4() {}
+
+void ok_funct2()
+{
+  if ((func_call()==0 ) ) {
+  }
+}
+
+void recognized_function5() {}
+
+void broken_funct2()
+{
+  if ((func_call()==0)) {
+goto EXIT;
+  }
+}
+
+void unrecognized_function1() {}
+
