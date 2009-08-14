eeb3612bcd1a16e5c335e9e445432973c5e81316
timeless <timeless@mozdev.org>
2009-08-14 10:18:03 +0200

    Bug 442166 doesn't detect class definitions reliably class recognition only recognized empty C++ classes (regression introduced 1999-01-20 when java support was added)

---
+++ tests/syntax-highlighting/class.h
@@ -0,0 +1,20 @@
+/* c++ class syntax highlighting */
+
+class class1;
+
+class class2 {
+
+};
+
+class class3 : public class2 {
+
+};
+
+class class4 {
+  friend class class5;
+};
+
+class class5 {
+  class class6 {
+  };
+};
