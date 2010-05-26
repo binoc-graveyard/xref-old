422ae369da5ca4c269ed02afcbd5900eca83a060
timeless <timeless@mozdev.org>
2010-05-26 18:55:15 +0200

    Bug 568254 improve chrome: include hint algorithm Improve source handling of chrome: urls to hint the whole path of the current file

--- lib/LXR/Common.pm
+++ lib/LXR/Common.pm
@@ -588,9 +588,10 @@ sub htmlquote {
 
 sub freetextmarkup {
   my $tree = $Conf->{'treename'} ne '' ? '/' . $Conf->{'treename'} : '';
+  my $virtf = $Path->{'virtf'};
 
   $_[0] =~ s#((?:ftp|https?|feed)://[^\s"'\)&<>]+)#<a href="$1">$1</a>#gi;
-  $_[0] =~ s#(chrome)(://)(\w+)(/)(\w+)(/[^&\s]+|)(/[^/\s"'\)&<>]+)#<a href="$tree/search?string=$3&find=contents.rdf">$1$2</a><a href="$tree/search?string=$3&find=chrome\\.manifest">$3$4</a><a href="$tree/search?string=$3&find=chrome\\.manifest&filter=$5">$5</a><a href="$tree/find?string=$7&hint=$3$6">$6$7</a>#gi;
+  $_[0] =~ s#(chrome)(://)(\w+)(/)(\w+)(/[^&\s]+|)(/[^/\s"'\)&<>]+)#<a href="$tree/search?string=$3&find=contents.rdf">$1$2</a><a href="$tree/search?string=$3&find=chrome\\.manifest">$3$4</a><a href="$tree/search?string=$3&find=chrome\\.manifest&filter=$5">$5</a><a href="$tree/find?string=$7&hint=$3$6$virtf">$6$7</a>#gi;
   $_[0] =~ s#(&amp;lt;(?:[Mm][Aa][Ii][Ll][Tt][Oo]:|)([^\s"']*?@[^\s"']*?)&amp;gt;)#<a href=\"mailto:$2\">$1</a>#g;
   $_[0] =~ s#(\((?:[Mm][Aa][Ii][Ll][Tt][Oo]:|)([^\s"']*?@[^\s"']*?)\))#<a href=\"mailto:$2\">$1</a>#g;
   $_[0] =~ s#(\0<(?:[Mm][Aa][Ii][Ll][Tt][Oo]:|)([^\s"']*?@[^\s"']*?)\0>)#<a href=\"mailto:$2\">$1</a>#g;
