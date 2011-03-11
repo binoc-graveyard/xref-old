776cdaaa2d72169f893f2717141847d80cb058ae
timeless <timeless@mozdev.org>
2011-03-11 10:15:47 +0000

    Support --by-unit in update-full-onetree

--- update-full-onetree.sh
+++ update-full-onetree.sh
@@ -2,12 +2,17 @@
 cd `dirname $0`
 TREE="$1"
 CRON=""
-if [ -n "$2" ] && [ "$1" == "-cron" ]; then
+UNIT=""
+if [ -n "$2" ] && [ "$1" = "-cron" ]; then
     TREE="$2"
     CRON="$1"
 fi
+if [ -n "$3" ] && [ "$2" = "--by-unit" ]; then
+    TREE="$3"
+    UNIT="$2"
+fi
 OUT1=`perl update-src.pl $CRON "$TREE" 2>&1`
-OUT2=`perl update-xref.pl $CRON "$TREE" 2>&1`
+OUT2=`perl update-xref.pl $CRON $UNIT "$TREE" 2>&1`
 OUT3=`perl update-search.pl $CRON "$TREE" 2>&1`
 if [ -n "$OUT1" ] || [ -n "$OUT2" ] || [ -n "$OUT3" ]; then
   echo "Updating $TREE..."
