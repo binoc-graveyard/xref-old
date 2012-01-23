66109df900a06721393b447c7857c1416e36aca5
nmaul <nmaul@mozilla.com>
2012-01-23 09:11:04 -0800

    Merging local changes to repo... a few months of updates.

--- update-full-onetree-tmp.sh
+++ update-full-onetree-tmp.sh
@@ -6,16 +6,21 @@ if [ -n "$2" ] && [ "$1" == "-cron" ]; then
     TREE="$2"
     CRON="$1"
 fi
+if [ "$TREE" == "amo-stage"]; then
+OUT5=`rsync -a --delete /data/mxr-data/amo/ /data/mxr-data/amo-backup 2>&1`
+fi
 OUT1=`perl update-src.pl $CRON "$TREE" 2>&1`
 OUT2=`perl update-xref.pl $CRON "$TREE" 2>&1`
 OUT3=`perl update-search.pl $CRON "$TREE" 2>&1`
 if [ "$TREE" == "amo-stage"]; then
 OUT4=`perl update-root.pl $CRON "$TREE" /data/mxr-data/amo/amo 2>&1`
 fi
-if [ -n "$OUT1" ] || [ -n "$OUT2" ] || [ -n "$OUT3" ]; then
+if [ -n "$OUT1" ] || [ -n "$OUT2" ] || [ -n "$OUT3" ] || [ -n "$OUT4" ] || [ -n "$OUT5" ]; then
   echo "Updating $TREE..."
+  echo "$OUT5"
   echo "$OUT1"
   echo "$OUT2"
   echo "$OUT3"
+  echo "$OUT4"
 fi
 
--- update-full-onetree.sh
+++ update-full-onetree.sh
@@ -11,9 +11,22 @@ if [ -n "$3" ] && [ "$2" = "--by-unit" ]; then
     TREE="$3"
     UNIT="$2"
 fi
+
+TREE_PATH=$(grep "^sourceroot: $TREE " /var/www/webtools/mxr/lxr.conf | head -n 1 | awk '{print $3}')
+
 OUT1=`perl update-src.pl $CRON "$TREE" 2>&1`
-OUT2=`perl update-xref.pl $CRON $UNIT "$TREE" 2>&1`
-OUT3=`perl update-search.pl $CRON "$TREE" 2>&1`
+
+if [ -f "${TREE_PATH}/last-processed" ] && [ -z "$(find ${TREE_PATH} -type f -newer ${TREE_PATH}/last-processed ! -path "*/.hg/*" ! -path "*/CVS/*" ! -path "${TREE_PATH}/.mozconfig.out" -print -quit)" ]; then
+	echo "$TREE: No files changed, skipping xref and indexing"
+	OUT2=''
+	OUT3=''
+else
+	echo "$TREE: Has changes, running xref and search..."
+	OUT2=`perl update-xref.pl $CRON $UNIT "$TREE" 2>&1`
+	OUT3=`perl update-search.pl $CRON "$TREE" 2>&1`
+	touch "${TREE_PATH}/last-processed"
+fi
+
 if [ -n "$OUT1" ] || [ -n "$OUT2" ] || [ -n "$OUT3" ]; then
   echo "Updating $TREE..."
   echo "$OUT1"
