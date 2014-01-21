44ddd79d95bb8a357a7b1358fefb2a1149fd76dc
Kendall Libby <klibby@mozilla.com>
2014-01-21 14:29:23 -0500

    Bug 961543, revert f6bc0c952c4d until a proper fix is found

--- lib/LXR/Common.pm
+++ lib/LXR/Common.pm
@@ -1668,7 +1668,7 @@ sub expandtemplate {
 # simply return an appropriate value.
 sub bannerexpand {
   if ($who eq 'source' || $who eq 'sourcedir' || $who eq 'diff') {
-    return htmlquote($Path->{'xref'});
+    return $Path->{'xref'};
   };
   return '';
 }
