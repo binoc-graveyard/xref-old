548e7065bf2bc5b3fc086fc9185b4cdaa6be5ecb
Kendall Libby <klibby@mozilla.com>
2014-06-27 10:46:06 -0400

    adding mozilla-esr31

--- update-src.pl
+++ update-src.pl
@@ -441,7 +441,7 @@ for ($TREE) {
         }
         last;
     };
-    /^(mozilla-esr10|mozilla-esr17|mozilla-esr24)$/ && do {
+    /^(mozilla-esr10|mozilla-esr17|mozilla-esr24|mozilla-esr31)$/ && do {
         if (-d "$src_dir/.hg") {
           hg_update($src_dir);
         } else {
